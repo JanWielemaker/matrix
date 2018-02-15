649ca243e61d35d944a401c0f2e27523eff251c6
Jan Wielemaker <j.wielemaker@cwi.nl>
2018-02-15 16:30:17 +0100

    Multi type, clean way

--- matrix.c
+++ matrix.c
@@ -1,45 +1,68 @@
 #include <stddef.h>
+#include <assert.h>
+
+typedef long long m_int;
+typedef double    m_float;
+
+typedef enum mtype
+{ ints,
+  floats
+} mtype;
 
 typedef struct matrix
 { int     ndim;
-  size_t  esize;				/* size of elements */
+  mtype   type;				/* Type of elements */
   size_t  dim[0];
 } matrix;
 
 #define SIZEOF_MHEADER(ndim) \
 	((size_t)(&((matrix*)NULL)->dim[ndim]))
 
-size_t
-matrix_size_1(size_t esize, size_t dim0)
-{ return SIZEOF_MHEADER(1) + esize*dim0;
+static inline size_t
+esize(mtype type)
+{ switch(type)
+  { case ints:   return sizeof(m_int);
+    case floats: return sizeof(m_float);
+    default:	 assert(0);
+  }
 }
 
 size_t
-matrix_size_2(size_t esize, size_t dim0, size_t dim1)
-{ return SIZEOF_MHEADER(1) + esize*dim0*dim1;
+matrix_size_1(mtype type, size_t dim0)
+{ return SIZEOF_MHEADER(1) + esize(type)*dim0;
+}
+
+size_t
+matrix_size_2(mtype type, size_t dim0, size_t dim1)
+{ return SIZEOF_MHEADER(1) + esize(type)*dim0*dim1;
 }
 
 void
-matrix_init_1(matrix *m, size_t esize, size_t dim0)
+matrix_init_1(matrix *m, mtype type, size_t dim0)
 { m->ndim   = 1;
+  m->type   = type;
   m->dim[0] = dim0;
-  m->esize  = esize;
 }
 
 void
-matrix_init_2(matrix *m, size_t esize, size_t dim0, size_t dim1)
+matrix_init_2(matrix *m, mtype type, size_t dim0, size_t dim1)
 { m->ndim   = 2;
+  m->type   = type;
   m->dim[0] = dim0;
   m->dim[1] = dim1;
-  m->esize  = esize;
 }
 
 size_t
 matrix_offset_1(const matrix *m, size_t i0)
-{ return SIZEOF_MHEADER(1) + m->esize * i0;
+{ return SIZEOF_MHEADER(1) + esize(m->type) * i0;
 }
 
 size_t
 matrix_offset_2(const matrix *m, size_t i0, size_t i1)
-{ return SIZEOF_MHEADER(1) + m->esize * (i0*m->dim[0] + i1);
+{ return SIZEOF_MHEADER(1) + esize(m->type) * (i0*m->dim[0] + i1);
+}
+
+mtype
+matrix_type(const matrix *m)
+{ return m->type;
 }
--- matrix.pl
+++ matrix.pl
@@ -14,11 +14,13 @@ c_define(mptr, *(struct(matrix))).
 
 :- c_import("#include \"matrix.c\"",
             [ matrix ],
-            [ matrix_size_1(int, int, [int]),
-              matrix_size_2(int, int, int, [int]),
+            [ matrix_size_1(enum, int, [int]),
+              matrix_size_2(enum, int, int, [int]),
 
-              matrix_init_1(mptr, int, int),
-              matrix_init_2(mptr, int, int, int),
+              matrix_init_1(mptr, enum, int),
+              matrix_init_2(mptr, enum, int, int),
+
+              matrix_type(mptr, [enum]),
 
               matrix_offset_1(mptr, int, [int]),
               matrix_offset_2(mptr, int, int, [int])
@@ -26,35 +28,43 @@ c_define(mptr, *(struct(matrix))).
 
 %!  matrix_new(+Type, +Dim, -Matrix) is det.
 
-matrix_new(ints, [Dim0], Matrix) :-
+matrix_new(Type, [Dim0], Matrix) :-
     !,
-    c_sizeof(int, Esize),
-    matrix_size_1(Esize, Dim0, Size),
+    matrix_size_1(Type, Dim0, Size),
     c_calloc(Matrix, struct(matrix), Size, 1),
-    matrix_init_1(Matrix, Esize, Dim0).
-matrix_new(ints, [Dim0, Dim1], Matrix) :-
-    c_sizeof(int, Esize),
-    matrix_size_2(Esize, Dim0, Dim1, Size),
+    matrix_init_1(Matrix, Type, Dim0).
+matrix_new(Type, [Dim0, Dim1], Matrix) :-
+    matrix_size_2(Type, Dim0, Dim1, Size),
     c_calloc(Matrix, struct(matrix), Size, 1),
-    matrix_init_2(Matrix, Esize, Dim0, Dim1).
+    matrix_init_2(Matrix, Type, Dim0, Dim1).
 
 %!  matrix_set(+Matrix, +Position, +Elem) is det.
 
 matrix_set(Matrix, [Dim0], Elem) :-
     !,
     matrix_offset_1(Matrix, Dim0, Offset),
-    c_store(Matrix, Offset, int, Elem).
+    matrix_ctype(Matrix, CType),
+    c_store(Matrix, Offset, CType, Elem).
 matrix_set(Matrix, [Dim0,Dim1], Elem) :-
     matrix_offset_2(Matrix, Dim0, Dim1, Offset),
-    c_store(Matrix, Offset, int, Elem).
+    matrix_ctype(Matrix, CType),
+    c_store(Matrix, Offset, CType, Elem).
 
 %!  matrix_get(+Matrix, +Position, -Elem) is det.
 
 matrix_get(Matrix, [Dim0], Elem) :-
     !,
     matrix_offset_1(Matrix, Dim0, Offset),
-    c_load(Matrix, Offset, int, Elem).
+    matrix_ctype(Matrix, CType),
+    c_load(Matrix, Offset, CType, Elem).
 matrix_get(Matrix, [Dim0,Dim1], Elem) :-
     matrix_offset_2(Matrix, Dim0, Dim1, Offset),
-    c_load(Matrix, Offset, int, Elem).
+    matrix_ctype(Matrix, CType),
+    c_load(Matrix, Offset, CType, Elem).
 
+matrix_ctype(Matrix, Type) :-
+    matrix_type(Matrix, Type0),
+    c_type(Type0, Type).
+
+c_type(ints, longlong).
+c_type(floats, double).
